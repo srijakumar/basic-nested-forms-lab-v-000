6bae2559517649bd20160815a9537badef58c6e0
Srija S <srijakumar7@gmail.com>
2019-05-26 15:47:14 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,20 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
+  end
+
+  def update
+    recipe = Recipe.find_by(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+  before_save :erase_empty_ingredients
+
+  def erase_empty_ingredients
+    self.ingredients = self.ingredients.select{|ingredient| ingredient.name && ingredient.name != '' ||ingredient.quantity && ingredient.quantity != '' }
+  end
 end
